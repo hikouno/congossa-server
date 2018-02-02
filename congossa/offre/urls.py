0f249dab30a9347e1fbad56d13bf7bcef9b332b1
jacquesRud <jacques.rudloff@gmail.com>
2018-02-02 10:31:55 +0100

    changement fction

--- congossa/offre/urls.py
+++ congossa/offre/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
-	# Slug pour les string jsp pourquoi ca a l air d etre de la triche un peu
 	# Routage vers les differentes fction
-    path('inscription/<slug:nom>', views.inscription, name='inscription'),
-    path('desincription/<slug:nom>', views.desincription, name='desincription'),
-    path('rechercher/<slug:nom>', views.rechercher, name='rechercher'),
+    path('', views.index, name='index'),
+	# Slug pour les string jsp pourquoi pas String ca a l air d etre de la triche un peu
+    path('ajoutOffre/<slug:nom>', views.ajoutOffre, name='ajoutOffre'),
+    path('ajoutDemande/<slug:nom>', views.ajoutDemande, name='ajoutDemande'),
+    path('voirMatch/<slug:nom>', views.voirMatch, name='voirMatch'),
 
 ]
--- congossa/offre/views.py
+++ congossa/offre/views.py
@@ -3,14 +3,14 @@ from django.http import HttpResponse
 
 
 #Les fonctions a appeler les parametres sont recuperer dans urls.py (nom dans mon cas)
-def inscription(request, nom):
-    return HttpResponse("You want to register as %s." % nom)
+def ajoutOffre(request, nom):
+    return HttpResponse("You want to ajouter Offre as %s." % nom)
 
-def desincription(request, nom):
-    return HttpResponse("You want to deregister as %s." % nom)
+def ajoutDemande(request, nom):
+    return HttpResponse("You want to ajouter demande as %s." % nom)
 
-def rechercher(request, nom):
-    return HttpResponse("You want to look at %s." % nom)
+def voirMatch(request, nom):
+    return HttpResponse("You want voir vos match as %s." % nom)
 	
 def index(request):
     return HttpResponse("You are in offre")
