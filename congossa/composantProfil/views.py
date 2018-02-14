8a4efb730f2dc4a1666c90d780626e16be847a14
jacquesRud <jacques.rudloff@gmail.com>
2018-02-14 16:37:04 +0100

    fction composant profil

--- congossa/composantProfil/views.py
+++ congossa/composantProfil/views.py
@@ -8,10 +8,21 @@ def CreateCompetence(contenu):
 	competence.save()
 	return HttpResponse("Competence %s cree" % contenu)
 ##########################
+def EditCompetence(competence,newContent):
+	competence.contenu=newContent
+	competence.save()
+	return HttpResponse("Competence %s medifie" % newContent)
+##########################
 def CreateNiveauEtude(duree,domaine):
 	niveauEtude=NiveauEtude.Create(duree,domaine)
 	niveauEtude.save()
 	return HttpResponse("Niveau d'etude  %s %s cree" % duree domaine)
+##########################
+def EditNiveauEtude(niveauEtude,duree,domaine):
+	niveauEtude.duree=duree
+	niveauEtude.domaine=domaine
+	niveauEtude.save()
+	return HttpResponse("niveauEtude %s medifie" % newContent)
 ##########################
 def CreateMetier(contenu):
 	metier=Metier.Create(contenu)
@@ -27,3 +38,11 @@ def CreateExperience(metier,dateDebut,dateFin):
 	experience=Experience.Create(metier,dateDebut,dateFin)
 	experience.save()
 	return HttpResponse("experience %s du %s au %s cree" % metier.intitule dateDebut dateFin)
+##########################
+def EditExperience(experience,metier,dateDebut,dateFin):
+	experience.metier=metier
+	experience.dateDebut=dateDebut
+	experience.dateFin=dateFin
+	experience.save()
+	return HttpResponse("experience %s du %s au %s cree" % metier.intitule dateDebut dateFin)
+##########################
