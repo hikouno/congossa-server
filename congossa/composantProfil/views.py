2637174765c4475ebbf388a4592118738797432e
jacquesRud <jacques.rudloff@gmail.com>
2018-02-14 15:35:19 +0100

    composant Profil

--- congossa/composantProfil/views.py
+++ congossa/composantProfil/views.py
@@ -2,15 +2,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+##########################
 def CreateCompetence(contenu):
 	competence=Competence.Create(contenu)
 	competence.save()
 	return HttpResponse("Competence %s cree" % contenu)
+##########################
 def CreateNiveauEtude(duree,domaine):
-	niveauEtude=Competence.Create(duree,domaine)
+	niveauEtude=NiveauEtude.Create(duree,domaine)
 	niveauEtude.save()
 	return HttpResponse("Niveau d'etude  %s %s cree" % duree domaine)
+##########################
 def CreateMetier(contenu):
-	metier=Competence.Create(contenu)
+	metier=Metier.Create(contenu)
 	metier.save()
 	return HttpResponse("Metier %s cree" % contenu)
+##########################
+def CreateQualite(contenu):
+	qualite=Qualite.Create(contenu)
+	qualite.save()
+	return HttpResponse("Metier %s cree" % contenu)
+##########################
+def CreateExperience(metier,dateDebut,dateFin):
+	experience=Experience.Create(metier,dateDebut,dateFin)
+	experience.save()
+	return HttpResponse("experience %s du %s au %s cree" % metier.intitule dateDebut dateFin)
