8483798c00f25f0a6492d3e36961c2e671a22af6
François <francois.havret@etu.enseeiht.fr>
2018-02-21 18:01:47 +0100

    modif

--- congossa/chat/views.py
+++ congossa/chat/views.py
@@ -1,48 +1,35 @@
 from django.shortcuts import render
-from django.views import generic
-from braces.views import LoginRequiredMixin
-
-try:
-    from django.urls import reverse
-except ImportError:
-    from django.core.urlresolvers import reverse
-from . import models
-from . import utils
 from django.shortcuts import get_object_or_404
-from django.contrib.auth import get_user_model
-from django.conf import settings
-from django.db.models import Q
+from django.http import JsonResponse
+from django.http import HttpResponse
 
 
-class DialogListView(LoginRequiredMixin, generic.ListView):
-    template_name = 'django_private_chat/dialogs.html'
-    model = models.Dialog
-    ordering = 'modified'
+## Returns dialog object if given two user ids
+def getDialogFromIds(login1, login2):
+    user1 = get_object_or_404(Utilisateur,username=login1) # récupération de l'objet user1
+    user2 = get_object_or_404(Utilisateur,username=login2) # récupération de l'objet user2
+    dialog = Dialog(owner=login1, opponent=login2)
 
-    def get_queryset(self):
-        dialogs = models.Dialog.objects.filter(Q(owner=self.request.user) | Q(opponent=self.request.user))
-        return dialogs
+    return dialog
+    
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data()
-        if self.kwargs.get('username'):
-            # TODO: show alert that user is not found instead of 404
-            user = get_object_or_404(get_user_model(), username=self.kwargs.get('username'))
-            dialog = utils.get_dialogs_with_user(self.request.user, user)
-            if len(dialog) == 0:
-                dialog = models.Dialog.objects.create(owner=self.request.user, opponent=user)
-            else:
-                dialog = dialog[0]
-            context['active_dialog'] = dialog
-        else:
-            context['active_dialog'] = self.object_list[0]
-        if self.request.user == context['active_dialog'].owner:
-            context['opponent_username'] = context['active_dialog'].opponent.username
-        else:
-            context['opponent_username'] = context['active_dialog'].owner.username
-        context['ws_server_path'] = '{}://{}:{}/'.format(
-            settings.CHAT_WS_SERVER_PROTOCOL,
-            settings.CHAT_WS_SERVER_HOST,
-            settings.CHAT_WS_SERVER_PORT,
-        )
-        return context
+## Add a message to the database when one is sent   
+def addMessage(request):
+    user1 = get_object_or_404(Utilisateur,username=login1) # récupération de l'objet user1
+    user2 = get_object_or_404(Utilisateur,username=login2) # récupération de l'objet user1
+    m = Message(dialog=getDialogFromIds(login1,login2), sender=user, text=message)
+    m.save()
+
+
+## Returns last 30messages of a dialog as a Json file
+def getDialog(request):
+    dialog = getDialogFromIds(login1,login2)
+    messages = dialog.message_set.all().order_by('timestamp') # getting messages corresponding to the dialog and ordering them
+    data =  sortMessages(messages)
+    return JsonResponse(data)
+
+def getLastMessage(request):
+    user = get_object_or_404(Utilisateur,username=login) # récupération de l'objet user
+
+def newDialog():
+
